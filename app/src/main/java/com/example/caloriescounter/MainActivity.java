94924f34e8def7c1978fc3aa8bea45bcb30c2af4
Sylvain4193 <Sylvain4193@gmail.com>
2021-04-29 14:13:29 +0200

    BottomBar
    Nav et bottomBar fonctionnel, strings fait

--- app/src/main/java/com/example/caloriescounter/MainActivity.java
+++ app/src/main/java/com/example/caloriescounter/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.caloriescounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomBar);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.diaryFragment, R.id.statsFragment).build();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
     }
 }
